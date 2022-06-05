e47a3874a72811b0204f9aa3a8e3cd83272b3a69
kokoro <cocosyu.11261126@gmail.com>
2022-06-05 21:12:25 +0900

    Add: svgアニメーションとリサイズイベントを追加

--- src/js/main.js
+++ src/js/main.js
@@ -4,13 +4,8 @@
  */
 
 // jsモジュールの読み込み
-import dummyModule from './modules/dummy.js'
-
-// tsモジュールの読み込み
-import dummyTsModule from './ts/dummy.ts'
+import './modules/resize'
+import './modules/svg'
 
 // scssを読み込み
 import '../scss/style.scss'
-
-dummyModule();
-console.log(dummyTsModule(3, 9));
